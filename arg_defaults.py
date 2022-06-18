090b7f5dafa786307e4e770accd0975f4d800fdc
Jason Hill <jason@acuitusag.com>
2022-06-17 22:01:04 -0600

    Use arg_defaults.py instead of command line parameters

---
+++ arg_defaults.py
@@ -0,0 +1,10 @@
+defaults = {
+  'da-json': 'J1939DA_MAY2022.json',
+  'format': True,
+  'json-array': True,
+  'input-separator': ',',
+  'timestamp-column': 0,
+  'link': True,
+  'include-na': True,
+  'transport': True
+}
--- run.sh
+++ run.sh
@@ -5,5 +5,4 @@ OUTPUT_FILENAME="tmp/output-$FILENAME.json"
 
 rm -rf "$OUTPUT_FILENAME"
 
-python3 pretty_j1939.py --input-separator ',' --timestamp-column 0 --format --link --include-na --transport --da-json J1939DA_MAY2022.json "$INPUT_FILENAME" > "$OUTPUT_FILENAME"
-# python3 pretty_j1939.py --input-separator ',' --timestamp-column 0 --format --candata                       --da-json J1939DA_MAY2022.json "$INPUT_FILENAME" > "$OUTPUT_FILENAME"
+python3 pretty_j1939.py "$INPUT_FILENAME" > "$OUTPUT_FILENAME"
